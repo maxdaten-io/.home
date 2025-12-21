4c5c066ad8403ce57a2adde99ea246885a9f8cf9
Jan-Philip Loos <maxdaten@gmail.com>
2025-12-21 04:25:19 +0100

    chore: Update darwin configuration

--- users/jloos/modules/claude-code.nix
+++ users/jloos/modules/claude-code.nix
@@ -3,14 +3,14 @@
   home.packages = with pkgs; [
     (pkgs.buildNpmPackage rec {
       pname = "claude-code";
-      version = "2.0.72";
+      version = "2.0.75";
 
       src = pkgs.fetchurl {
         url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-        hash = "sha256-MeDnwe98890zSqRvqqePzrzYirKrLNmE2P2wL9lxaK0=";
+        hash = "sha256-gQzf+LG/wdIdsWSIfNkSrk+NQYI9ITZKAs0UeNE7g2I=";
       };
 
-      npmDepsHash = "sha256-mOKPjjzN0bRTJ7+B/zY5MYMgcIqyMbTMVBJvLHk36pM=";
+      npmDepsHash = "sha256-qjFhnH73w2BZl1LrexT1bxBxj6IJdbCiHbklp4Dm+qY=";
 
       # Get with `npm install @anthropic-ai/claude-code --package-lock-only`
       postPatch = ''
