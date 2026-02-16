469d52a3c6cf905ab465a3f5a5f63fa471e2f935
Jan-Philip Loos <maxdaten@gmail.com>
2026-02-16 03:01:15 +0100

    chore: Update darwin configuration

--- users/jloos/modules/zoxide.nix
+++ users/jloos/modules/zoxide.nix
@@ -9,4 +9,15 @@
   programs.fish.shellAliases = {
     cd = "z";
   };
+
+  programs.fish.functions = {
+    gz = ''
+      if test (count $argv) -eq 0
+        open -a Ghostty $PWD
+      else
+        set -l dir (zoxide query $argv)
+        and open -a Ghostty $dir
+      end
+    '';
+  };
 }
